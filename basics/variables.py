c03a03d5058ba9df6e2085bc2226bf9927a2e935
VrajMistry01 <mistryvraj0001@gmail.com>
2026-04-18 12:24:48 +0530

    feat:variables and identity vs equality-v2

--- basics/variables.py
+++ basics/variables.py
@@ -4,10 +4,20 @@
 # d=500
 # print(a is b)
 # print(c is d)
-a = 50
-b = 50
-print(id(a), id(b), a is b)
+# a = 50
+# b = 50
+# print(id(a), id(b), a is b)
 
-c = 500
-d = 500
-print(id(c), id(d), c is d)
+# c = 500
+# d = 500
+# print(id(c), id(d), c is d)
+
+def a():
+    a=500
+    print(id(a))
+    def b():
+        b=500
+        print(id(b))
+        print(a is b)  #Same file → same compilation context → Python optimizes identical literals to one object. Same behavior we saw before.
+    return b
+a()()
