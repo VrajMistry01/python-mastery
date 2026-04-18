781093a274561e445a829842ee372ca49c34994e
VrajMistry01 <mistryvraj0001@gmail.com>
2026-04-18 12:44:23 +0530

    feat: basic data types and bool-int relationship

---
+++ basics/data_types.py
@@ -0,0 +1,10 @@
+# Data Types in Python
+print(type(10))
+print(type(3.14))
+print(type("hello"))
+print(type(True))
+print(type(None))
+
+# Bool is subclass of int
+print(isinstance(True, int))
+print(True + True + False)
