72e1ac44c7d7faf5e203111b26270146696f9a38
VrajMistry01 <mistryvraj0001@gmail.com>
2026-04-19 14:51:18 +0530

    fixed : string task-v2, covered all edge case

--- basics/string_task.py
+++ basics/string_task.py
@@ -2,8 +2,10 @@ def username_generator(fullname : str) -> str:
     #clean the fnaeme
     clean_name = fullname.strip().lower()
     #replace the spaces with underscores
-    username = clean_name.replace(" ","_")
-    username = username+'_'+str(len(clean_name))
+    split_name = clean_name.split() #split without arg will split at space and remove the leading spaces automatically
+    if not split_name:
+        return ("User name cannot be empty")
+    username = "_".join(split_name)+'_'+str(len(clean_name))
     return username
 test_list= ["  Vraj Mistry  ", "  John Doe  ", "  Jane Smith  ","Alice","   ","  John  Doe  "]
 for name in test_list:
