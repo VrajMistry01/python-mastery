52907960f3036c7e56317cc490b85667ac8b33b7
VrajMistry01 <mistryvraj0001@gmail.com>
2026-04-20 00:20:53 +0530

    task -v2:Conditional Grading Systemand a multi if reward system with ternary condtion and truthy - falsy

--- basics/conditionals.py
+++ basics/conditionals.py
@@ -4,6 +4,7 @@ def grading_system(marks:int)->str:
 
     if marks > 90:
         print('GRADE A')
+    
     elif marks > 70:
         print("GRADE B")
     else:
@@ -12,3 +13,22 @@ def grading_system(marks:int)->str:
 list_marks = [80,95,20,90]
 for marks in list_marks:
     grading_system(marks)
+# An example showing the difference between multiple ifs determining rewards 
+
+def reward_system(marks:int)->str:
+    if marks>90:
+        print('Gold Reward')
+    if marks>70:
+        print('Silver Reward')
+    if marks>50:
+        print('Participation Reward')
+    if marks <=50:
+        print('No rewards') if marks >=0 else print('Invalid marks')
+    if not marks:
+        print("0 marks.. Try again next time!")
+ 
+list1_marks = [80,95,20,90,-10,0]
+
+for marks in list1_marks:
+    reward_system(marks)
+
